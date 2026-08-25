29f338c417ef0c6cdd655dc92f642595ea0928d6
Lhes05 <lesicalla12345678@outlook.com>
2026-08-25 12:12:12 +0800

    Add main JavaScript file for SanteShop Engine

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* =================================
+   SanteShop Global Engine
+   Main JavaScript
+   Version: 0.1
+================================= */
+
+console.log("SanteShop Engine Loaded 🚀");
